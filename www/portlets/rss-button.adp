34c21b0bd0bbf33bd93f4ed47f6841f3228d56f8
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2006-05-05 10:57:03 +0000

    allow to specify time span for rss button

--- www/portlets/rss-button.adp
+++ www/portlets/rss-button.adp
@@ -1,1 +1,1 @@
-<a href='@instance@?rss' class='rss'>RSS</a>
+<a href='@instance@?rss=@rss_param@' class='rss'>RSS</a>
--- www/portlets/rss-button.tcl
+++ www/portlets/rss-button.tcl
@@ -1,4 +1,5 @@
-
+# use "span" to specify parameters to the rss call
+set rss_param [expr {[info exists span] ? $span : "10d"}]
 set folder_id  [$__including_page set parent_id]
 set package_id [$folder_id set package_id]
 set instance [site_node::get_url_from_object_id -object_id $package_id]
