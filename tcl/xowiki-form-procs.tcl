2e7f2b77f50beecd5094a8ab56e618d4c344ce35
Dave Bauer <dave@thedesignexperience.org>
2006-11-30 20:10:04 +0000

    Set default value for production mode parameter

--- tcl/xowiki-form-procs.tcl
+++ tcl/xowiki-form-procs.tcl
@@ -160,7 +160,7 @@ namespace eval ::xowiki {
     if {[$data exists_form_parameter text.format]} {
       $data set mime_type [$data form_parameter text.format]
     }
-    if {[[$data set package_id] get_parameter production_mode]} {
+    if {[[$data set package_id] get_parameter production_mode "f"]} {
       $data set content_item.publish_status production
     }
   }
