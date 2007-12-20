d391d6c2e826ffd14034e54330d23fcdc445f823
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2007-12-20 14:29:40 +0000

    make date convertion for podcast more robust against local environment variable settings

--- tcl/xowiki-form-procs.tcl
+++ tcl/xowiki-form-procs.tcl
@@ -475,7 +475,7 @@ namespace eval ::xowiki {
     if {$widgetinfo ne ""} {
       foreach {y m day hour min} $widgetinfo break
       set t [clock scan "${hour}:$min $m/$day/$y"]
-      return [clock format $t]
+      return [clock format $t -format "%y-%m-%d %T"]
     }
     return ""
   }
