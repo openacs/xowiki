4106c8ab49966db380c3be539c239ea80d025220
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2008-09-08 10:33:08 +0000

    - provide view link (when in edit mode) - provide <A ... text=''> for wiki menu to improve accessability - text values added to message catalog

--- tcl/adp-generator-procs.tcl
+++ tcl/adp-generator-procs.tcl
@@ -69,15 +69,16 @@ function get_popular_tags(popular_tags_link, prefix) {
   ADP_Generator instproc wikicmds_part {} {
     if {![my wikicmds]} {return ""}
     return {<div id='wikicmds'>
-  <if @edit_link@ not nil><a href="@edit_link@" accesskey='e' title='Diese Seite bearbeiten ...'>#xowiki.edit#</a> &middot; </if>
-  <if @rev_link@ not nil><a href="@rev_link@" accesskey='r'>#xotcl-core.revisions#</a> &middot; </if>
-  <if @new_link@ not nil><a href="@new_link@" accesskey='n'>#xowiki.new_page#</a> &middot; </if>
-  <if @delete_link@ not nil><a href="@delete_link@" accesskey='d'>#xowiki.delete#</a> &middot; </if>
-  <if @admin_link@ not nil><a href="@admin_link@" accesskey='a'>#xowiki.admin#</a> &middot; </if>
-  <if @notification_subscribe_link@ not nil><a href='/notifications/manage'>#xowiki.notifications#</a> 
+  <if @view_link@ not nil><a href="@view_link@" accesskey='v' title='#xowiki.view_title#'>#xowiki.view#</a> &middot; </if>
+  <if @edit_link@ not nil><a href="@edit_link@" accesskey='e' title='#xowiki.edit_title#'>#xowiki.edit#</a> &middot; </if>
+  <if @rev_link@ not nil><a href="@rev_link@" accesskey='r' title='#xowiki.revisions_title#'>#xotcl-core.revisions#</a> &middot; </if>
+  <if @new_link@ not nil><a href="@new_link@" accesskey='n' title='#xowiki.new_title#'>#xowiki.new_page#</a> &middot; </if>
+  <if @delete_link@ not nil><a href="@delete_link@" accesskey='d' title='#xowiki.delete_title#'>#xowiki.delete#</a> &middot; </if>
+  <if @admin_link@ not nil><a href="@admin_link@" accesskey='a' title='#xowiki.admin_title#'>#xowiki.admin#</a> &middot; </if>
+  <if @notification_subscribe_link@ not nil><a href='/notifications/manage' title='#xowiki.notifications_title#'>#xowiki.notifications#</a> 
       <a href="@notification_subscribe_link@">@notification_image;noquote@</a> &middot; </if>  
-   <a href='#' onclick='document.getElementById("do_search").style.display="inline";document.getElementById("do_search_q").focus(); return false;'>#xowiki.search#</a> &middot;
-  <if @index_link@ not nil><a href="@index_link@" accesskey='i'>#xowiki.index#</a></if>
+   <a href='#' onclick='document.getElementById("do_search").style.display="inline";document.getElementById("do_search_q").focus(); return false;'  title='#xowiki.search_title#'>#xowiki.search#</a> &middot;
+  <if @index_link@ not nil><a href="@index_link@" accesskey='i' title='#xowiki.index_title#'>#xowiki.index#</a></if>
 <div id='do_search' style='display: none'> 
   <FORM action='/search/search'><div><INPUT id='do_search_q' name='q' type='text'><INPUT type="hidden" name="search_package_id" value="@package_id@" ></div></FORM> 
 </div>
