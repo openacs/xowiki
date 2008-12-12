4cee717d7de74eb4020f6584454ac8eb7a6f4f40
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2008-12-12 13:49:07 +0000

    - deletegate delete to package_object to ensure cache flushing

--- www/admin/delete-type.tcl
+++ www/admin/delete-type.tcl
@@ -15,10 +15,10 @@
 set sql [$object_type instance_select_query -with_subtypes 0 -folder_id [::$package_id folder_id]]
 db_foreach retrieve_instances $sql {
   permission::require_write_permission -object_id $item_id
-  $object_type delete -item_id $item_id
+  $package_id delete -item_id $item_id -name $name
 }
 
 # drop type requires that all pages of all xowiki instances are deleted
-#foreach type [$object_type object_types -subtypes_first true] {$type drop_object_type}
+# foreach type [$object_type object_types -subtypes_first true] {$type drop_object_type}
 
 ad_returnredirect $return_url
