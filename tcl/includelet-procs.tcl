50b04e6a0546247a768f760b4e881192f217a45f
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2011-12-27 15:30:29 +0000

    - refactor "book" includelet to ease overloading via mixins - provided a small includelet for w3c slidy, mostly compatible with "book" includelet

--- tcl/includelet-procs.tcl
+++ tcl/includelet-procs.tcl
@@ -2394,7 +2394,9 @@ namespace eval ::xowiki::includelet {
 
     return $inner_html$save_form
   }
+}
 
+namespace eval ::xowiki::includelet {
   #############################################################################
   # book style
   #
@@ -2414,62 +2416,50 @@ namespace eval ::xowiki::includelet {
         }}
       }
 
-  book instproc render {} {
-    my get_parameters
 
-    my instvar __including_page
-    lappend ::xowiki_page_item_id_rendered [$__including_page item_id]
-    $__including_page set __is_book_page 1
-
-    set allow_reorder [my page_reorder_check_allow $allow_reorder]
-
-    set extra_where_clause ""
-    set cnames ""
-    if {[info exists category_id]} {
-      foreach {cnames extra_where_clause} [my category_clause $category_id] break
+  book instproc render_item {
+    -menu_buttons 
+    -content:required 
+    -object:required 
+    -level:required
+  } {
+    $object instvar page_order title name
+    set menu [list]
+    foreach b $menu_buttons {
+      if {[info command ::xowiki::includelet::$b] eq ""} {
+	set b $b-item-button
+      }
+      set html [$object include [list $b -book_mode true]]
+      if {$html ne ""} {lappend menu $html}
     }
-
-    foreach {locale locale_clause} \
-        [::xowiki::Includelet locale_clause -revisions p -items p $package_id $locale] break
-
-    if {$folder_mode} {
-      # TODO just needed for michael aram?
-      set parent_id [$__including_page item_id]
-    } else {
-      #set parent_id [$package_id folder_id]
-      set parent_id [$__including_page parent_id]
+    set menu [join $menu "&nbsp;"]
+    if {$menu ne ""} {
+      # <div> not allowed in h*: style='float: right; position: relative; top: -32px
+      set menu "<span style='float: right;'>$menu</span>"
     }
+    append output \
+	"<h$level class='book'>" $menu \
+	"<a name='[toc anchor $name]'></a>$page_order $title</h$level>" \
+	$content
+  }
 
-    set pages [::xowiki::Page instantiate_objects -sql \
-        "select page_id, page_order, name, title, item_id \
-		from xowiki_page_live_revision p \
-		where parent_id = $parent_id  \
-		and not page_order is NULL $extra_where_clause \
-		$locale_clause \
-		[::xowiki::Page container_already_rendered item_id]" ]
-    $pages mixin add ::xo::OrderedComposite::IndexCompare
-    $pages orderby page_order
-
+  book instproc render_items {
+    -pages:required 
+    {-cnames ""} 
+    {-allow_reorder ""}
+    -menu_buttons 
+    {-with_footer "false"}
+  } {
     set output ""
     if {$cnames ne ""} {
       append output "<div class='filter'>Filtered by categories: $cnames</div>"
     }
-    set return_url [::xo::cc url]
-
-    if {$range ne ""} {
-      foreach {from to} [split $range -] break
-      foreach p [$pages children] {
-	if {[$pages __value_compare [$p set page_order] $from 0] == -1
-	    || [$pages __value_compare [$p set page_order] $to 0] > 0} {
-	  $pages delete $p
-	}
-      }
-    }
 
     my page_reorder_init_vars -allow_reorder $allow_reorder js last_level ID min_level
+    set renderer default
 
     foreach o [$pages children] {
-      $o instvar page_order title page_id name title 
+      $o instvar page_order page_id
       set level [expr {[regsub -all {[.]} $page_order _ page_order_js] + 1}]
 
       if {$allow_reorder ne ""} {
@@ -2513,25 +2503,12 @@ namespace eval ::xowiki::includelet {
           #set content [string map [list "\{\{" "\\\{\{"] $content]
         }
       }
-      set menu [list]
-      foreach b $menu_buttons {
-	if {[info command ::xowiki::includelet::$b] eq ""} {
-	  set b $b-item-button
-	}
-	set html [$p include [list $b -book_mode true]]
-	if {$html ne ""} {lappend menu $html}
-      }
-      set menu [join $menu "&nbsp;"]
-      if {$menu ne ""} {
-        # <div> not allowed in h*: style='float: right; position: relative; top: -32px
-        set menu "<span style='float: right;'>$menu</span>"
-      }
-
-      append output \
-          "<h$level class='book'>" $menu \
-          "<a name='[toc anchor $name]'></a>$page_order $title</h$level>" \
-          $content
-
+      
+      append output [my render_item \
+			 -menu_buttons $menu_buttons \
+			 -content $content \
+			 -object $p \
+			 -level $level]
       if {$with_footer} {
         append output [$p htmlFooter -content $content]
       }
@@ -2541,11 +2518,136 @@ namespace eval ::xowiki::includelet {
       for {set l $last_level} {$l > 0} {incr l -1} {append output "</ul>\n" }
       append output "<script type='text/javascript'>$js</script>\n"
     }
+    return $output
+  }
 
+  book instproc render {} {
+    my get_parameters
+
+    my instvar __including_page
+    lappend ::xowiki_page_item_id_rendered [$__including_page item_id]
+    $__including_page set __is_book_page 1
+
+    set allow_reorder [my page_reorder_check_allow $allow_reorder]
+
+    set extra_where_clause ""
+    set cnames ""
+    if {[info exists category_id]} {
+      foreach {cnames extra_where_clause} [my category_clause $category_id] break
+    }
+
+    foreach {locale locale_clause} \
+        [::xowiki::Includelet locale_clause -revisions p -items p $package_id $locale] break
+
+    if {$folder_mode} {
+      # TODO just needed for michael aram?
+      set parent_id [$__including_page item_id]
+    } else {
+      #set parent_id [$package_id folder_id]
+      set parent_id [$__including_page parent_id]
+    }
+
+    set pages [::xowiki::Page instantiate_objects -sql \
+        "select page_id, page_order, name, title, item_id \
+		from xowiki_page_live_revision p \
+		where parent_id = $parent_id  \
+		and not page_order is NULL $extra_where_clause \
+		$locale_clause \
+		[::xowiki::Page container_already_rendered item_id]" ]
+    $pages mixin add ::xo::OrderedComposite::IndexCompare
+    $pages orderby page_order
+
+    #
+    # filter range
+    #
+    if {$range ne ""} {
+      foreach {from to} [split $range -] break
+      foreach p [$pages children] {
+	if {[$pages __value_compare [$p set page_order] $from 0] == -1
+	    || [$pages __value_compare [$p set page_order] $to 0] > 0} {
+	  $pages delete $p
+	}
+      }
+    }
+
+    if {[llength [$pages children]] < 1} {
+      #
+      # Provide a hint why not pages were found
+      #
+      set p [::xo::db::CrClass get_instance_from_db -item_id $parent_id]
+      set output "<p>No pages with parent object [$p name], page_order not NULL and an appropriate publish status found</p>\n"
+    } else {
+      set output [my render_items \
+		      -menu_buttons $menu_buttons \
+		      -with_footer $with_footer \
+		      -pages $pages \
+		      -cnames $cnames \
+		      -allow_reorder $allow_reorder]
+    }
     return $output
   }
 }
 
+namespace eval ::xowiki::includelet {
+  #############################################################################
+  # book style
+  #
+  ::xowiki::IncludeletClass create slidy \
+      -superclass ::xowiki::includelet::book \
+      -parameter {
+        {__decoration plain}
+        {parameter_declaration {
+          {-category_id}
+          {-menu_buttons edit}
+	  {-folder_mode false}
+          {-locale ""}
+	  {-range ""}
+	  {-allow_reorder ""}
+	  {-with_footer "false"}
+        }}
+      }
+  slidy instproc render_items {
+    -pages:required 
+    {-cnames ""} 
+    {-allow_reorder ""}
+    -menu_buttons 
+    {-with_footer "false"}
+  } {
+    if {$cnames ne "" || $allow_reorder ne "" || $with_footer ne "false"} {
+      error "ignoring cnames, allow_reorder, and with_footer for the time being"
+    }
+
+    my instvar __including_page
+
+    set output [subst {<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"> 
+<head> 
+      <title>[$__including_page title]</title> 
+  <link rel="stylesheet" href="http://www.w3.org/Talks/Tools/Slidy2/styles/slidy.css" type="text/css" media="screen, projection" />
+  <link rel="stylesheet" href="print.css" type="text/css"
+  media="print" />
+  <script src="http://www.w3.org/Talks/Tools/Slidy2/scripts/slidy.js" type="text/javascript">
+  </script>
+</head>
+    }]
+    append output "<body>\n"
+    foreach o [$pages children] {
+      $o instvar page_order page_id
+       set p [::xo::db::CrClass get_instance_from_db -item_id 0 -revision_id $page_id]
+
+      set content [$p render -with_footer false]
+
+      append output "<div class='slide'>\n" $content "\n</div>\n"
+    }
+    append output "</body>\n"
+    ns_return 200 text/html $output
+    ad_script_abort
+    #return $output
+  }
+}
+
+#############################################################################
+# item-button
+#
 namespace eval ::xowiki::includelet {
   ::xowiki::IncludeletClass create item-button \
       -superclass ::xowiki::Includelet \
@@ -2736,8 +2838,6 @@ namespace eval ::xowiki::includelet {
                   -object_type $object_type]
     }
   }
-
-
 }
 
 
