d3d0aac62255620de143353c84a5f84f8de8202e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-03-30 21:58:10 +0000

    - fix logic for validity check for tag links

--- tcl/package-procs.tcl
+++ tcl/package-procs.tcl
@@ -1378,8 +1378,8 @@ namespace eval ::xowiki {
 	set tag $stripped_url
 	set summary [::xo::cc query_parameter summary 0]
 	set popular [::xo::cc query_parameter popular 0]
-	if {[string is boolean -strict $summary]} {error "summary must be boolean"}
-	if {[string is boolean -strict $popular]} {error "popular must be boolean"}
+	if {![string is boolean -strict $summary]} {error "summary must be boolean"}
+	if {![string is boolean -strict $popular]} {error "popular must be boolean"}
 	set tag_kind [expr {$popular ? "ptag" :"tag"}]
 	set weblog_page [my get_parameter weblog_page]
 	my get_lang_and_name -default_lang $default_lang -name $weblog_page (lang) (stripped_name)
