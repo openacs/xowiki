1f07a5d0cb0f5786854dfe73ca9f77e78883f561
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2015-02-11 11:29:55 +0000

    - added plogin syntaxhighlight - reduced size of default menubar

--- www/resources/ckeditor4/build-config.js
+++ www/resources/ckeditor4/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/54be9581a90d68a577bb17bd29369e63
+ * (2) http://ckeditor.com/builder/e6fdea1185eb08e8b11607de6b1235a5
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/54be9581a90d68a577bb17bd29369e63
+ * (3) http://ckeditor.com/builder/download/e6fdea1185eb08e8b11607de6b1235a5
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'bootstrapck',
-	preset: 'full',
+	preset: 'standard',
 	ignore: [
 		'.bender',
 		'bender.js',
@@ -52,62 +52,39 @@ var CKBUILDER_CONFIG = {
 		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
 		'contextmenu' : 1,
-		'dialogadvtab' : 1,
-		'div' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
-		'find' : 1,
-		'flash' : 1,
 		'floatingspace' : 1,
-		'font' : 1,
 		'format' : 1,
-		'forms' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'iframe' : 1,
 		'iframedialog' : 1,
 		'image' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
-		'language' : 1,
-		'lineutils' : 1,
 		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
 		'magicline' : 1,
-		'mathjax' : 1,
 		'maximize' : 1,
-		'newpage' : 1,
-		'pagebreak' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
-		'preview' : 1,
-		'print' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
-		'save' : 1,
 		'scayt' : 1,
-		'selectall' : 1,
 		'showblocks' : 1,
 		'showborders' : 1,
-		'smiley' : 1,
 		'sourcearea' : 1,
-		'sourcedialog' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
+		'syntaxhighlight' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'widget' : 1,
