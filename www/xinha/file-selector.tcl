6d7cf007f13a296939a6807054bb691bd440250b
Victor Guerra <vguerra@gmail.com>
2015-03-05 15:49:37 +0000

    Casting text constants on query and using plpsql function to check permission instead of using a subquery (takes advantage of recursive permissions implementation)

--- www/xinha/file-selector.tcl
+++ www/xinha/file-selector.tcl
@@ -262,9 +262,9 @@ set fs_sql "select object_id, name, live_revision, type, title,
            to_char(last_modified, 'YYYY-MM-DD HH24:MI:SS') as last_modified_ansi,
            content_size, url, sort_key, file_upload_name,
            case
-             when :folder_path is null
+             when :folder_path::text is null
              then fs_objects.name
-             else :folder_path || '/' || name
+             else :folder_path::text || '/' || name
            end as file_url,
            case
              when last_modified >= (now() - cast('99999' as interval))
@@ -273,11 +273,7 @@ set fs_sql "select object_id, name, live_revision, type, title,
            end as new_p
         from fs_objects
         where parent_id = :folder_id
-        and exists (select 1
-           from acs_object_party_privilege_map m
-           where m.object_id = fs_objects.object_id
-             and m.party_id = :user_id
-             and m.privilege = 'read')
+        and acs_permission__permission_p(fs_objects.object_id,:user_id,'read')='t'
          $filter_clause
          $order_by_clause"
 
