6b1db6df65413f57c6e8bd2c61c6f78709a7065c
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-02-28 10:41:35 +0000

    - added helper "::xowiki::get_raw_request_body" to obtain the content of the request body either -as_string or as_file (abstracts spool file handling away)

--- tcl/xowiki-utility-procs.tcl
+++ tcl/xowiki-utility-procs.tcl
@@ -392,9 +392,37 @@ namespace eval ::xowiki {
   }
   proc write_file {fn content} {
     set F [::open $fn w]
-    ::fconfigure $F -translation binary
+    ::fconfigure $F -translation binary -encoding binary
     ::puts -nonewline $F $content
     ::close $F
+  }
+  
+  nsf::proc ::xowiki::get_raw_request_body {-as_string:switch -as_file:switch} {
+    if {$as_string eq $as_file} {
+      error "either -as_string or -as_file must be specified"
+    }
+    set contentfile [ns_conn contentfile]
+    if {$as_file} {
+      #
+      # If the file was not spooled, obtained it via [ns_conn content]
+      # as write it to a file.
+      #
+      if {$contentfile eq ""} {
+        set contentfile [ad_tmpnam]
+        write_file $contentfile [ns_conn content -binary]
+      }
+      set result $contentfile
+    } else {
+      #
+      # Return the result as a string
+      #
+      if {$contentfile eq ""} {
+        set result [ns_conn content -binary]
+      } else {
+        set result [read_file $contentfile]
+      }
+    }
+    return $result
   }
 
   proc ::xowiki::page_order_uses_ltree {} {
