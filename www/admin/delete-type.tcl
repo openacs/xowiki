4132461b2724f11a80a319ba3b8bb52371eb76fa
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-09-13 07:09:54 +0000

    - use "www-" prefix

--- www/admin/delete-type.tcl
+++ www/admin/delete-type.tcl
@@ -15,7 +15,7 @@
 set sql [$object_type instance_select_query -with_subtypes 0 -folder_id [::$package_id folder_id]]
 xo::dc foreach retrieve_instances $sql {
   permission::require_write_permission -object_id $item_id
-  $package_id delete -item_id $item_id -name $name
+  $package_id www-delete -item_id $item_id -name $name
 }
 
 # drop type requires that all pages of all xowiki instances are deleted
