33b99ab07f5d0ce3d2196376a8ca2190a1d492a0
Hector Romojaro <hector.romojaro@gmail.com>
2016-09-27 14:23:20 +0000

    Fixed typo mime_time -> mime_type

--- tcl/form-field-procs.tcl
+++ tcl/form-field-procs.tcl
@@ -883,7 +883,7 @@ namespace eval ::xowiki::formfield {
   } {
 
     set content_type_registered [::xo::dc 1row check_content_type {
-      select exists(select 1 from cr_mime_types where mime_time = :content_type)
+      select exists(select 1 from cr_mime_types where mime_type = :content_type)
     }]
     #
     # If the provided mime type is not registered, or unknown, try
