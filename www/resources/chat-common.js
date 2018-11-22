7906aec5ebd9c2029e99c9041d44fa5f4ce57db6
Antonio Pisano <antonio@elettrotecnica.it>
2018-11-22 09:15:37 +0000

    Don't use the "javascript:" presudo-URL idiom to avoid CSP complaints from the browser

--- www/resources/chat-common.js
+++ www/resources/chat-common.js
@@ -13,7 +13,7 @@ function addFullScreenLink() {
     // Full screen trigger
     const triggerFSlink = document.createElement("a");
     triggerFSlink.className = 'xowiki-chat-trigger-fs-link';
-    triggerFSlink.setAttribute("href", "javascript:void(0)");
+    triggerFSlink.setAttribute("href", "#");
 
     // Trigger icon
     var triggerFSoff = '<svg class="xowiki-chat-trigger-fs-pic" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 14" preserveAspectRatio="xMidYMin meet"><g stroke-width=".033882"><path d="m4.994 10.008v2.1596l-0.6017-0.602-0.6017-0.601-1.0668 1.068c-0.58675 0.58697-1.0823 1.0672-1.1012 1.0672-0.04453 0-1.5755-1.5315-1.5755-1.576 0-0.0186 0.49551-0.52914 1.1011-1.1345l1.1011-1.1007-0.71966-0.72024-0.71966-0.72024h4.1838z"/><path d="m9.7119 8.5856-0.71966 0.72024 1.0842 1.0838c0.5963 0.59609 1.0842 1.0993 1.0842 1.1182 0 0.01889-0.3507 0.38481-0.77933 0.81314l-0.7791 0.779-1.0758-1.076-1.0759-1.075-0.6182 0.618-0.6182 0.618v-4.3196h2.1089 2.1089z"/><path d="m2.8589 2.4267 1.0838 1.0842 0.60168-0.60099 0.60168-0.60099v2.1258 2.1258h-4.0148l0.6182-0.6182 0.6182-0.6182-1.1602-1.1606-1.1602-1.1606 0.82964-0.83016c0.4563-0.45658 0.84509-0.83016 0.86398-0.83016 0.01888 0 0.52204 0.48787 1.1181 1.0842z"/><path d="m10.331 2.1727 0.82964 0.83016-1.1602 1.1606-1.1602 1.1606 0.6182 0.6182 0.6182 0.6182h-3.9814v-4.3194l0.61862 0.61794 0.61862 0.61794 1.0668-1.0672c0.58675-0.58697 1.0747-1.0672 1.0842-1.0672 0.0096 0 0.39074 0.37357 0.84704 0.83016z"/></g></svg>';
@@ -21,7 +21,8 @@ function addFullScreenLink() {
     triggerFSlink.innerHTML = triggerFSon;
 
     // Full screen when clicked
-    triggerFSlink.addEventListener("click", function() {
+    triggerFSlink.addEventListener("click", function(event) {
+        event.preventDefault();
         var chat = document.getElementById('xowiki-chat');
         var chatFS = document.getElementById('xowiki-chat-fs');
         if (chat != null) {
