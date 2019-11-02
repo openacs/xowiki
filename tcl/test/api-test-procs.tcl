51d25dff7b7bea92e21e1f913c766d579533b3b9
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-11-02 15:46:40 +0000

    improve naming

--- tcl/test/api-test-procs.tcl
+++ tcl/test/api-test-procs.tcl
@@ -3,39 +3,38 @@ aa_register_case \
     -procs {
         "::xowiki::Package instproc normalize_path"
     } \
-    package_api_calls {
+    package_normalize_path {
 
-    Checks various API calls on package level
+        Checks various forms of the xowiki::Package API method
+        "normalize_path".
 
-    @author Gustaf Neumann
-} {
-
-    set package_id [acs::test::require_package_instance \
-                        -package_key xowiki]
-    ::xowiki::Package initialize -package_id $package_id
-
-    #
-    # Don't allow addressing outside of the jail
-    #
-    foreach pair {
-        {"view-default" "view-default"}
-        {"view-default/." "view-default"}
-        {"./view-default/." "view-default"}
-        {"../view-default/." "view-default"}
-        {"../../view-default/." "view-default"}
-        {"/../../view-default/." "view-default"}
-        {".." ""}
-        {"/../../view-default/../" ""}
-        {"/etc/hosts" "etc/hosts"}
-        {"//etc/hosts" "etc/hosts"}
-        {"/../etc/hosts" "etc/hosts"}
-        {"view-default/../../etc" "etc"}
-        {"view-default/../../../../../etc" "etc"}
     } {
-        lassign $pair path expected
-        aa_equals "check $path -> $expected" [$package_id normalize_path $path] $expected
+        set package_id [acs::test::require_package_instance \
+                            -package_key xowiki]
+        ::xowiki::Package initialize -package_id $package_id
+
+        #
+        # Don't allow addressing outside of the jail
+        #
+        foreach pair {
+            {"view-default" "view-default"}
+            {"view-default/." "view-default"}
+            {"./view-default/." "view-default"}
+            {"../view-default/." "view-default"}
+            {"../../view-default/." "view-default"}
+            {"/../../view-default/." "view-default"}
+            {".." ""}
+            {"/../../view-default/../" ""}
+            {"/etc/hosts" "etc/hosts"}
+            {"//etc/hosts" "etc/hosts"}
+            {"/../etc/hosts" "etc/hosts"}
+            {"view-default/../../etc" "etc"}
+            {"view-default/../../../../../etc" "etc"}
+        } {
+            lassign $pair path expected
+            aa_equals "check $path -> $expected" [$package_id normalize_path $path] $expected
+        }
     }
-}
 
 #
 # Local variables:
