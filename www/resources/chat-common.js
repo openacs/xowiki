dd53f06a3d3c72fd0ed6ef8c64bf5991a240f479
Hector Romojaro <hector.romojaro@gmail.com>
2020-04-02 18:09:49 +0000

    Add "Web notifications" for new messages in the chat. See: https://www.w3.org/TR/notifications/
        "You should only request consent to display notifications in response to a user gesture
        (e.g. clicking a button). This is not only best practice — you should not be spamming
        users with notifications they didn't agree to — but going forward browsers will explicitly
        disallow notification permission requests not triggered in response to a user gesture.
        Firefox is already doing this from version 72, for example, and Safari has done it for some time."
    
        https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API

--- www/resources/chat-common.js
+++ www/resources/chat-common.js
@@ -15,6 +15,53 @@ function windowFocus() {
     notifications = 0;
 }
 
+// Add "Web notifications" of new messages
+// https://www.w3.org/TR/notifications/
+function checkNotificationPromise() {
+    try {
+        Notification.requestPermission().then();
+    } catch(e) {
+        return false;
+    }
+    return true;
+}
+function askNotificationPermission() {
+    var notificationBtn = document.getElementById('enableNotifications');
+    // function to actually ask the permissions
+    function handlePermission(permission) {
+        // Whatever the user answers, we make sure Chrome stores the information
+        if(!('permission' in Notification)) {
+            Notification.permission = permission;
+        }
+
+        // set the button to shown or hidden, depending on what the user answers
+        if(Notification.permission === 'denied' || Notification.permission === 'default') {
+            notificationBtn.style.display = 'block';
+        } else {
+            notificationBtn.style.display = 'none';
+        }
+    }
+
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+        console.log("This browser does not support notifications.");
+    } else {
+        if(checkNotificationPromise()) {
+            Notification.requestPermission()
+                .then((permission) => {
+                    handlePermission(permission);
+                })
+        } else {
+            Notification.requestPermission(function(permission) {
+                handlePermission(permission);
+            });
+        }
+    }
+}
+window.onload = function () {
+    document.getElementById('enableNotifications').addEventListener("click", askNotificationPermission);
+}
+
 // Retrieve user_id
 function chatGetMyUserId() {
     var my_user = document.getElementById('xowiki-my-user-id');
@@ -51,6 +98,9 @@ function createLink(text) {
 
 // Render the data, being a user or a message
 function renderData(json) {
+    var notificationBtn = document.getElementById('enableNotifications');
+notificationBtn.addEventListener("click", askNotificationPermission);
+
     if (json.type == "message") {
         renderMessage(json);
         // Produce tab notification
@@ -58,6 +108,11 @@ function renderData(json) {
             notifications++;
             var newTitle = '(' + notifications + ') ' + title;
             document.title = newTitle;
+            // Web notification
+            var user_id = json.user_id;
+            var text = json.user.replace(/\\'/g, "\"") + ": " + json.message;
+            var img = "/shared/portrait-bits.tcl?user_id=" + user_id;
+            var notification = new Notification(title, { body: text, icon: img });
         }
     } else if (json.type == "users") {
         renderUsers(json);
