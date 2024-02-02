ced984398485a87ce6e680f8981f73740eab1f41
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-02-02 12:31:36 +0000

    Extended site-wide admin page to include link for site-wide pages
    Adding a link to site-wide admin pages makes it easier for admins to locate
    the site-wide pages (pages, which can be used in all xowiki instances)

--- www/sitewide-admin/index.adp
+++ www/sitewide-admin/index.adp
@@ -3,5 +3,9 @@
 <property name="context">@context;literal@</property>
 
 <h1>@title;noquote@</h1>
+<p>The pages available for all package instances @package_key@ are available here:
+<a href='/acs-admin/site-wide/@package_key@'>@package_key@  site-wide pages</a>.
+
+<h2>@resource_title;noquote@</h2>
 <p>Checking for <strong>@resoure_name@</strong> in version @version@.
 <include src="/packages/acs-tcl/lib/check-installed" &=resource_info &=version &=download_url>
--- www/sitewide-admin/index.tcl
+++ www/sitewide-admin/index.tcl
@@ -4,10 +4,12 @@ ad_page_contract {
     @creation-date July, 2020
 } {
 }
+set title "[::xowiki::Package pretty_name] - Site-wide Admin"
+set package_key [::xowiki::Package package_key]
 set version 1.2.0
 set resource_info [::xowiki::bootstrap_treeview::resource_info -version $version]
 set resoure_name  [dict get $resource_info resourceName]
-set title "$resoure_name - Sitewide Admin"
+set resource_title "Static resources"
 set download_url download?version=$version
 set context [list $title]
 
